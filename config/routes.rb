dd70b41c3f36d756b36a95e9665ac03e50c5de63
Ruben Arakelyan <ruben@arakelyan.uk>
2024-04-03 16:49:29 +0100

    Add Auth0 authentication

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "dashboard#index"
+
+  get "/auth/auth0/callback" => "auth0#callback"
+  get "/auth/failure" => "auth0#failure"
+  get "/auth/logout" => "auth0#logout"
 end
